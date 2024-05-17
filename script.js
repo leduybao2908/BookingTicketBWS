cf2717991253f77ff4db9195ac9305029296fc3b
Chinh2903 <nubibo123@gmail.com>
2024-05-17 21:58:05 +0700

    change Flight Tickets

--- script.js
+++ script.js
@@ -86,3 +86,16 @@ document.getElementById("xemthembuttonPQ").addEventListener("click", function() 
 
 
 
+function dynamicDropDown(listIndex) {
+
+    document.getElementById("infants").length = 0;
+    document.getElementById("children").length = 0;
+  
+    for (let i = 0; i < Number(listIndex) + 1; i++) {
+      document.getElementById("infants").options[i] = new Option(i.toString(), i);
+    }
+  
+    for (let i = 0; i < 9 - Number(listIndex) + 1; i++) {
+      document.getElementById("children").options[i] = new Option(i.toString(), i);
+    }
+  }
